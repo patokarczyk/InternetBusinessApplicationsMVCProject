3a13b54dd7fa782dc4437361ab1ed9be4fc0a96e
Pawel Tokarczyk <ptokarczyk22@gmail.com>
2026-04-22 15:08:59 +0200

    feat(Databease ready): entire database and crud views ready

---
+++ MyCompany/MyCompany.Intranet/Migrations/20260422123504_EmployeeMigration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyCompany.Intranet.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    IdEmployee = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    IdWarehouse = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.IdEmployee);
+                    table.ForeignKey(
+                        name: "FK_Employee_Warehouse_IdWarehouse",
+                        column: x => x.IdWarehouse,
+                        principalTable: "Warehouse",
+                        principalColumn: "IdWarehouse",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_IdWarehouse",
+                table: "Employee",
+                column: "IdWarehouse");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
