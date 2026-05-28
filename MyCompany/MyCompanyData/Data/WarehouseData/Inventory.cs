f5ba69b0f4ef2c6ef43e1e111ddef5add9b66199
Pawel Tokarczyk <ptokarczyk22@gmail.com>
2026-05-28 16:41:26 +0200

    feat(After)

---
+++ MyCompany/MyCompanyData/Data/WarehouseData/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyCompanyData.Data.WarehouseData
+{
+    public class Inventory
+    {
+        [Key]
+        public int IdInventory { get; set; }
+
+        [ForeignKey("Product")]
+        public int IdProduct { get; set; }
+
+        public Product Product { get; set; }
+
+        [ForeignKey("Warehouse")]
+        public int IdWarehouse { get; set; }
+
+        public Warehouse Warehouse { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
---
+++ MyCompany/MyCompanyData/Data/WarehouseData/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyCompanyData.Data.WarehouseData
+{
+    public class Order
+    {
+        [Key]
+        public int IdOrder { get; set; }
+
+        [ForeignKey("Supplier")]
+        public int IdSupplier { get; set; }
+
+        public Supplier Supplier { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [MaxLength(50)]
+        public string Status { get; set; }
+    }
+}
